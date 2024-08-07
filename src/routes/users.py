e064f4bc2112ab2162001eeded2c2667ac392899
francobko <franco.condor@urp.edu.pe>
2024-08-07 09:52:21 -0500

    feat: Add user routes to app

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,22 +1,29 @@
 from fastapi import APIRouter
+from config.db import conn
+from models.users import users
 
 user = APIRouter()
 
+
 @user.get('/')
-async def read_users():
-    return {'msg': 'Read Users'}
+async def get_users():
+    return conn.execute(users.select()).fetchall()
+
 
 @user.get('/{user_id}')
 async def read_user(user_id: int):
     return {'msg': f'Read User {user_id}'}
 
+
 @user.post('/')
 async def create_user():
     return {'msg': 'Create User'}
 
+
 @user.put('/{user_id}')
 async def update_user(user_id: int):
     return {'msg': f'Update User {user_id}'}
+
 
 @user.delete('/{user_id}')
 async def delete_user(user_id: int):
